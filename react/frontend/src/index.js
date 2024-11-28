698f9d82abda0ad10123681acc5f881dcc99ab98
eFrim1 <eleazarfrim@gmail.com>
2024-11-28 23:10:52 +0200

    minor changes

--- react/frontend/src/index.js
+++ react/frontend/src/index.js
@@ -7,17 +7,23 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 import {ColorModeScript} from "@chakra-ui/react";
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
- root.render(
-   <React.StrictMode>
-       <ColorModeScript initialColorMode={'dark'} />
+root.render(
+    <React.StrictMode>
+        <ColorModeScript initialColorMode={'dark'}/>
 
-     <BrowserRouter>
-     <App />
-     </BrowserRouter>
-   </React.StrictMode>
- );
+        <BrowserRouter>
+            <DevSupport ComponentPreviews={ComponentPreviews}
+                        useInitialHook={useInitial}
+            >
+                <App/>
+            </DevSupport>
+        </BrowserRouter>
+    </React.StrictMode>
+);
 
 
 // If you want to start measuring performance in your app, pass a function
